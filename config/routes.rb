a1764ed80f36baf1c1abc8c5a3070930240d1ae2
Liu Jiyang <2411mail@gmail.com>
2014-12-10 20:02:22 +0800

    comment works

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :blogs
+  resources :blogs do
+    resources :comments
+  end
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
