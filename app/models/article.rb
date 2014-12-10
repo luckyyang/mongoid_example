97e9531e9c06657dec89b6425af2888fb61181f7
Liu Jiyang <2411mail@gmail.com>
2014-12-10 17:29:50 +0800

    add comment model

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article
   field :name, type: String
   field :content, type: String
   field :published_on, type: Date
+  has_many :comments, :as => :commentable
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,6 @@ class Blog
   include Mongoid::Document
   field :title, type: String
   field :content, type: String
+
+  has_many :comments, :as => :commentable
 end
